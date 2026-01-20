b3542044d8b961ad11957e42fe67a2495edd333e
Jimmy Grafström <jimmy.grafstrom@gmail.com>
2026-01-20 10:13:30 +0100

    init commit

---
+++ src/main/java/se/jimmy/iths/Main.java
@@ -0,0 +1,26 @@
+package se.jimmy.iths;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+//starta webbservern
+        var server = new org.eclipse.jetty.server.Server(8080);
+//i denna context kan man registrera servlets
+        var context = new org.eclipse.jetty.servlet.ServletContextHandler();
+
+        server.setHandler(context);
+
+        context.addServlet(RootServlet.class, "/");
+//starta servern
+
+//starta servern
+        context.addServlet(HelloServlet.class, "/hello");
+
+        context.addServlet(HelloServlet.class, "/hello/*");//lägg till /*
+
+
+        server.start();
+        server.join();
+
+
+    }
+}
